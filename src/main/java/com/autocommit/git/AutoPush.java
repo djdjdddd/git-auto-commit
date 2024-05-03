b68d26852f47cae1a7969e1c52b81a1099444bec
djdjdddd <djdj2297@gmail.com>
2024-05-04 01:14:05 +0900

    auto commit test

--- src/main/java/com/autocommit/git/AutoPush.java
+++ src/main/java/com/autocommit/git/AutoPush.java
@@ -1,6 +1,6 @@
 package com.autocommit.git;
 
-import com.autocommit.git.user.MyConfig;
+import com.autocommit.git.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.RemoteAddCommand;
@@ -15,14 +15,18 @@ import java.net.URISyntaxException;
 @Slf4j
 public class AutoPush {
 
+    // TODO. 하루에 한번 실행하게 하려면 어떻게 해야 되지??
+
     private static String ORIGIN = "origin";
     private static String MAIN = "main";
 
     public static void main(String[] args) {
-
-        // 깃 사용자 정보를 담은 객체
-//        log.info("user = {}", user);
-
+        // TODO. 파일 타입(생성/수정/삭제)을 인식해서 커밋 컨벤션 자동으로 생성되게 할 수 없을까??
+        // 생성 -> Feat: Xxx 생성
+        // 삭제 -> Remove: Xxx 삭제
+        
+        
+        // TODO. 코드 리팩토링
         // 로컬 레포와 리모트 레포를 연결하는 코드
         String folderPath = "C:\\Users\\SSS\\IdeaProjects\\git";
         try {
@@ -30,7 +34,8 @@ public class AutoPush {
             RemoteAddCommand remoteAddCommand = git.remoteAdd();
             remoteAddCommand.setName(ORIGIN);
 
-            System.out.println("open pass");
+            System.out.println("open pass");    // TODO. 중간 진행 정도를 체크하여 만약 에러가 났을 경우 어디서 났는지 쉽게 체크되도록
+                                                // TODO. 단계별 시각화 안되나??
 
             // 여기에는 리모트 레포의 경로를 넣어주자
             remoteAddCommand.setUri(new URIish("https://github.com/djdjdddd/git-auto-commit.git"));
@@ -50,12 +55,13 @@ public class AutoPush {
 
             // git push origin main 역할
             git.push()
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(MyConfig.username, MyConfig.accessToken))
+                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(User.username, User.accessToken))
                     .setRemote(ORIGIN).add(MAIN)
                     .call();
 
             System.out.println("push pass");
         } catch (IOException | GitAPIException | URISyntaxException e) {
+            // TODO. Exception 처리 조금 더 세분화
             log.error("error", e);
             throw new RuntimeException(e);
         }
--- src/main/java/com/autocommit/git/user/User.java
+++ src/main/java/com/autocommit/git/user/User.java
@@ -1,18 +1,36 @@
 package com.autocommit.git.user;
 
-import lombok.Builder;
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
-@Builder
-@Getter
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@Slf4j
 public class User {
 
-    @Value("${github.username}")
-    private String userName;
+    public static String username;
 
-    @Value("${github.access-token}")
-    private String password;
+    public static String accessToken;
+
+    private static String GITHUB_ID = "github.id";
+
+    private static String GITHUB_ACCESS_TOKEN = "github.access-token";
+
+    private static String PROPERTIES_FILE_PATH = "src/main/resources/application.properties";
+
+    private static Properties properties;
+
+    static {
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream(PROPERTIES_FILE_PATH));
+            username = properties.getProperty(GITHUB_ID);
+            accessToken = properties.getProperty(GITHUB_ACCESS_TOKEN);
+        } catch (IOException e) {
+            log.error("error", e);
+            throw new RuntimeException(e);
+        }
+    }
 
 }
