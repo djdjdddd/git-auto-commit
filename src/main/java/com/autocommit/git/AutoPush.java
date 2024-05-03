61642e2d9562c41367a323726778db28c6ed351b
djdjdddd <djdj2297@gmail.com>
2024-05-04 00:17:36 +0900

    auto commit test

--- src/main/java/com/autocommit/git/AutoPush.java
+++ src/main/java/com/autocommit/git/AutoPush.java
@@ -33,21 +33,31 @@ public class AutoPush {
             RemoteAddCommand remoteAddCommand = git.remoteAdd();
             remoteAddCommand.setName(ORIGIN);
 
+            System.out.println("open 패스");
+
             // 여기에는 리모트 레포의 경로를 넣어주자
             remoteAddCommand.setUri(new URIish("https://github.com/djdjdddd/git-auto-commit.git"));
+
+            System.out.println("setUri 패스");
 
             // git add . 의 역할
             git.add().addFilepattern(".").call();
             remoteAddCommand.call();
 
+            System.out.println("add 패스");
+
             // git commit -M "커밋 메시지" 역할
             git.commit().setMessage("auto commit test").call();
+
+            System.out.println("commit 패스");
 
             // git push origin main 역할
             git.push()
                     .setCredentialsProvider(new UsernamePasswordCredentialsProvider(user.getUserName(), user.getPassword()))
                     .setRemote(ORIGIN).add(MAIN)
                     .call();
+
+            System.out.println("push 패스");
         } catch (IOException | GitAPIException | URISyntaxException e) {
             log.error("error", e);
             throw new RuntimeException(e);
